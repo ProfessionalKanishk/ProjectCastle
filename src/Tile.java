c72bd3e5a605e018554d134eb6490ec302a9c367
aykzhang <allenzhang42@gmail.com>
2018-10-20 19:16:57 -0500

    board

---
+++ src/Tile.java
@@ -0,0 +1,19 @@
+
+public class Tile 
+{
+	private Piece resident;
+	
+	public Tile(Piece piece)
+	{
+		resident = piece;
+	}
+	public Piece getResident() 
+	{
+		return resident;
+	}
+	public Piece setResident(Piece piece)
+	{
+		resident = piece;
+		return resident;
+	}
+}
