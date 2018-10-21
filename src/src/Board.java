86faa0fe7a3dc50b03a16735c1ba02c840a24796
MARK ELLIS <mark.m.ellis@utexas.edu>
2018-10-20 22:34:52 -0500

    basic check to the validity of a move in the board class

--- src/src/Board.java
+++ src/src/Board.java
@@ -13,59 +13,158 @@ public class Board
 			for(int j = 0; j < 8; j++)
 			{
 				if((i == 0 && j == 0)||(i == 0 && j == 7)||(i == 7 && j == 0)||(i == 7 && j == 7)) 
-					board[i][j] = new Tile(new Piece(1));
+					board[i][j] = new Tile(new Piece(1), i, j);
 				else if((i == 0 && j == 1)||(i == 0 && j == 6)||(i == 7 && j == 1)||(i == 7 && j == 6))
-					board[i][j] = new Tile(new Piece(2));
+					board[i][j] = new Tile(new Piece(2), i, j);
 				else if((i == 0 && j == 2)||(i == 0 && j == 5)||(i == 7 && j == 2)||(i == 7 && j == 5))
-					board[i][j] = new Tile(new Piece(3));
+					board[i][j] = new Tile(new Piece(3), i, j);
 				else if((i == 0 && j == 3)||(i == 7 && j == 3))
-					board[i][j] = new Tile(new Piece(4));
+					board[i][j] = new Tile(new Piece(4), i, j);
 				else if((i == 0 && j == 4)||(i == 7 && j == 4))
-					board[i][j] = new Tile(new Piece(5));
+					board[i][j] = new Tile(new Piece(5), i, j);
 				else if(i == 1 || i == 6)
-					board[i][j] = new Tile(new Piece(0));
+					board[i][j] = new Tile(new Piece(0), i, j);
 				else
 					board[i][j] = null;
 			}
 	}
 	
-	public boolean valid_move(Tile current, Tile dest) {
+	public boolean valid_move(Tile curr, Tile dest) {
+		int dx = 0;
+		int dy = 0;
+		if(dest.get_x() > 0 && dest.get_x() < 8 && dest.get_y() > 0 && dest.get_y() < 8) {
+			dx = curr.get_x() - dest.get_x();
+			dy = curr.get_y() - dest.get_y();
+		}
 		
+		else {
+			System.out.print("Incorrect destination");
+			return false;
+		}
 		//Pawn move validity
-	    if(current.getResident().getType() == 0) {
+	    if(curr.getResident().getType() == 0) {
 			
+			if (dx == 1 && dy == 1) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
+				
+			if(dx == 1) {
+				return true;
+			}
+			
+			return false;
 		}
 	    
-	    //Pawn move validity
-	    else if(current.getResident().getType() == 1) {
+	    //Rook move validity
+	    else if(curr.getResident().getType() == 1) {
+			if(curr.get_x() - dest.get_x() == 0 && curr.get_y() - dest.get_y() != 0) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
 			
+			else if(curr.get_x() - dest.get_x() != 0 && curr.get_y() - dest.get_y() == 0) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
+			
+			return false;
 		}
 
-	    //Pawn move validity
-	    else if(current.getResident().getType() == 2) {
+	    //Knight move validity
+	    else if(curr.getResident().getType() == 2) {
+			if(abs(dx) == 1 && abs(dy) == 2) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
 			
+			else if(abs(dx) == 2 && abs(dy) == 2) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
+			
+			return false;
 		}
 
-	    //Pawn move validity
-	    else if(current.getResident().getType() == 3) {
+	    //Bishop move validity
+	    else if(curr.getResident().getType() == 3) {
+			if(abs(dx) == abs(dy)) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
 			
+			return false;
 		}
 	    
-	    //Pawn move validity
-	    else if(current.getResident().getType() == 4) {
+	    //Queen move validity
+	    else if(curr.getResident().getType() == 4) {
+			if(abs(dx) == abs(dy)) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
 			
+			else if (abs(dx) == 0 && abs(dy) > 0) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
+			
+			else if (abs(dx) > 0 && abs(dy) == 0) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
+			
+			return false;
 		}
 
-	    //Pawn move validity
-	    else if(current.getResident().getType() == 5) {
+	    //King move validity
+	    else if(curr.getResident().getType() == 5) {
+			if(abs(dx) == 1 && abs(dy) == 1) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
 			
+			else if(abs(dx) == 1 && abs(dy) == 0) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
+			
+			else if(abs(dx) == 0 & abs(dy) == 1) {
+				dest.setResident(curr.getResident());
+				curr.setResident(null);
+				return true;
+			}
+			
+			return false;
 		}
+	    
+	    return false;
+	}
+	
 	public Tile[][] getBoard()
 	{
 		return board;
 	}
+	
 	public int getLevel() 
 	{
 		return level;
+	}
+	
+	public int abs(int x) {
+		if(x < 0) {
+			return x * -1;
+		}
+		return x;
 	}
 }
--- src/src/Tile.java
+++ src/src/Tile.java
@@ -2,18 +2,40 @@ package src;
 public class Tile 
 {
 	private Piece resident;
+	private int x_pos;
+	private int y_pos;
 	
-	public Tile(Piece piece)
+	public Tile(Piece piece, int x, int y)
 	{
 		resident = piece;
+		this.x_pos = x;
+		this.y_pos = y;
 	}
+	
 	public Piece getResident() 
 	{
 		return resident;
 	}
+	
 	public Piece setResident(Piece piece)
 	{
 		resident = piece;
 		return resident;
+	}
+	
+	public void set_x(int x) {
+		this.x_pos = x;
+	}
+	
+	public int get_x() {
+		return this.x_pos;
+	}
+	
+	public void set_y(int y) {
+		this.x_pos = y;
+	}
+	
+	public int get_y() {
+		return this.x_pos;
 	}
 }
